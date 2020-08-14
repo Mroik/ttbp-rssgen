3b1d7d193a87a6642832c97fd5d4604025bdb063
Mroik <mroik@delayed.space>
2020-08-14 22:54:29 +0200

    Removed unecessary '>>'

--- rssgen.sh
+++ rssgen.sh
@@ -9,25 +9,31 @@ else
 fi
 
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" > $location
-echo "<rss version=\"2.0\">" >> $location
-echo "<channel>" >> $location
-echo "<title>~$user on TTBP</title>" >> $location
-echo "<link>http://tilde.town/~$user/blog/</link>" >> $location
-echo "<updated>$(date)</updated>" >> $location
-echo "<description>$user's blog on tilde.town</description>" >> $location
+{
+	echo "<rss version=\"2.0\">"
+	echo "<channel>"
+	echo "<title>~$user on TTBP</title>"
+	echo "<link>http://tilde.town/~$user/blog/</link>"
+	echo "<updated>$(date)</updated>"
+	echo "<description>$user's blog on tilde.town</description>"
+} >> $location
 
 for x in $(ls /home/$user/.ttbp/entries)
 do
-	echo "<item>" >> $location
-	echo "<title>$(echo $x | sed -E s/\.txt//g)</title>">> $location
-	echo "<pubDate>$(echo $x | awk '{printf substr($0,1,4) "-" substr($0,5,2) "-" substr($0,7,2)}')</pubDate>" >> $location
-	echo "<link>http://tilde.town/~$user/blog/$(echo $x | sed -E s/\.txt/\.html/)</link>" >> $location
-	echo "<description><![CDATA[$(cat /home/$user/.ttbp/entries/$x)]]></description>" >> $location
-	echo "<author>$user@tilde.town</author>" >> $location
-	echo "</item>" >> $location
+	{
+		echo "<item>"
+		echo "<title>$(echo $x | sed -E s/\.txt//g)</title>"
+		echo "<pubDate>$(echo $x | awk '{printf substr($0,1,4) "-" substr($0,5,2) "-" substr($0,7,2)}')</pubDate>"
+		echo "<link>http://tilde.town/~$user/blog/$(echo $x | sed -E s/\.txt/\.html/)</link>"
+		echo "<description><![CDATA[$(cat /home/$user/.ttbp/entries/$x)]]></description>"
+		echo "<author>$user@tilde.town</author>"
+		echo "</item>"
+	} >> $location
 done
 
-echo "</channel>" >> $location
-echo "</rss>" >> $location
+{
+	echo "</channel>"
+	echo "</rss>"
+} >> $location
 
 echo "Finished generating rss feed... you can now go to $(echo $location | sed -E "s/^.*public_html/http:\/\/tilde\.town\/~$user/")"
